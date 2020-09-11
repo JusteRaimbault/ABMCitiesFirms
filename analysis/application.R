a3708dbee079063d07347e2efe5595824fe50588
Juste Raimbault <juste.raimbault@polytechnique.edu>
2020-09-11 08:16:58 +0100

    application_results

--- analysis/application.R
+++ analysis/application.R
@@ -6,11 +6,17 @@ library(ggplot2)
 
 source(paste0(Sys.getenv('CS_HOME'),'/Organisation/Models/Utils/R/plots.R'))
 
+resPrefix = '20200910_111006_APPLICATION'
+resdir = paste0('Results/Application/',resPrefix,'/');dir.create(resdir,recursive = T)
+
 #res <- as.tbl(read.csv(file='model_nl6/exploration/20200904_032159_APPLICATION.csv'))
 #res <- as.tbl(read.csv(file='model_nl6/exploration/20200904_092344_APPLICATION.csv'))
-res <- as.tbl(read.csv(file='model_nl6/exploration/20200910_111006_APPLICATION.csv'))
+res <- as.tbl(read.csv(file=paste0('model_nl6/exploration/',resPrefix,'.csv')))
 
 params = c("countryScaleFactor","gravityScaleFactor","id")
+indicators = c("internationalization","metropolization","networkAvgCommunitySize","networkDegreeEntropy",
+               "flowsHierarchyAlpha","rhoDegreeSize","rhoFlowDistance","networkDegreeHierarchyAlpha"
+)
 
 tmax = 98
 tstep = 500
@@ -23,10 +29,16 @@ for(t in seq(from=0,to=tmax,by=1)){
   tdata=rbind(tdata,cbind(res[,params],internationalization = inter,
                           networkAvgCommunitySize = avgsize,
                           rhoDegreeSize =rhoDegreeSize ,
-                          time = rep(t,nrow(res))
+                          time = rep(t*tstep,nrow(res))
                           ))
 }
 
+ylabs = indicators;names(ylabs)<-indicators
+ylabs[["internationalization"]] = "Internationalisation"
+ylabs[["rhoDegreeSize"]] = "Metropolisation"
+ylabs[["networkAvgCommunitySize"]] = "Average community size"
+
+tdata$countryScaleFactorString = paste0('kappa[C]*"="*',tdata$countryScaleFactor)
 
 g=ggplot(tdata,aes(x=time,y=internationalization,color=gravityScaleFactor,group=gravityScaleFactor))
 g+geom_smooth()+facet_wrap(~countryScaleFactor)
@@ -36,6 +48,16 @@ g+geom_smooth()+facet_wrap(~countryScaleFactor)
 
 g=ggplot(tdata,aes(x=time,y=rhoDegreeSize,color=gravityScaleFactor,group=gravityScaleFactor))
 g+geom_smooth()+facet_wrap(~countryScaleFactor)
+
+
+# targeted plots
+for(indicator in c("internationalization","rhoDegreeSize","networkAvgCommunitySize")){
+  g=ggplot(tdata[tdata$countryScaleFactor%in%c(0.2,1.0),],aes_string(x="time",y=indicator,color="gravityScaleFactor",group="gravityScaleFactor"))
+  g+geom_smooth()+facet_wrap(~countryScaleFactorString,labeller = label_parsed)+geom_vline(col='red',linetype=2,xintercept = tmax * tstep / 2)+
+    scale_color_continuous(name=expression(kappa[G]))+ylab(ylabs[[indicator]])+xlab("Time")+stdtheme
+  ggsave(file=paste0(resdir,indicator,'_Time_colorgravityScaleFactor_facetcountryScaleFactor.png'),width=30,height=15,units='cm')
+}
+
 
 
 # statistical tests
