7fc4f9dfc314f5744394dca7a41b6fe35f47affc
Juste Raimbault <juste.raimbault@ign.fr>
2022-04-21 11:13:37 +0200

    calib_macro:plots-script

--- analysis/calibration.R
+++ analysis/calibration.R
@@ -64,7 +64,39 @@ for(param in parameters){
 
 
 
+#####
+# Macro calibration
 
+
+resdirpref='CALIBRATION_MACRO_GRID_20220331_134531/'
+generation='8000'
+resdir=paste0('Results/Calibration/',resdirpref);dir.create(resdir)
+
+res <- as_tibble(read.csv(paste0('openmole/calibration/',resdirpref,'/population',generation,'.csv')))
+
+res$relErrorMetropolisation = res$objective.errorMetropolisation / 0.96
+res$relErrorInternationalisation = res$objective.errorInternationalisation / 0.32
+
+objectives = c("relErrorMetropolisation","relErrorInternationalisation")
+parameters = c("gravityDecay","countryGravityDecay","gammaSectors","gammaLinks","gammaOrigin","gammaDestination","finalTime")
+
+paramnames = list(gammaOrigin = expression(gamma[O]),
+                  gammaDestination = expression(gamma[D]),
+                  gammaLinks=expression(gamma[W]),
+                  gammaSectors=expression(gamma[S]),
+                  gravityDecay=expression(d[0]),
+                  countryGravityDecay=expression(c[0]),
+                  finalTime=expression(t[f])
+)
+
+
+
+for(param in parameters){
+  g=ggplot(res[res$evolution.samples>=10,],aes_string(x="relErrorMetropolisation",y="relErrorInternationalisation",color=param,size='evolution.samples'))
+  g+geom_point(alpha=0.6)+xlab("Relative error on Metropolisation")+ylab("Relative error on Internationalisation")+
+    scale_color_continuous(name=paramnames[[param]])+scale_size_continuous(name='Samples')+stdtheme
+  ggsave(paste0(resdir,'/pareto_color',param,'.png'),width=20,height=18,units='cm')
+}
 
 
 
