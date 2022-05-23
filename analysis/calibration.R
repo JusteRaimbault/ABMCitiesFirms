9ebc967759b5de39e6ebe8aab932950646451dd2
Juste Raimbault <juste.raimbault@ign.fr>
2022-05-23 17:52:01 +0200

    calib_results

--- analysis/calibration.R
+++ analysis/calibration.R
@@ -1,30 +1,42 @@
 
 setwd(paste0(Sys.getenv("CS_HOME"),'/UrbanDynamics/Models/ABMCitiesFirms'))
 
-library(dplyr)
+library(dplyr,warn.conflicts = F)
 library(ggplot2)
 
 source(paste0(Sys.getenv("CS_HOME"),'/Organisation/Models/Utils/R/plots.R'))
 
 
-
-resdirpref='CALIBRATION_GRID_20200305_092250/'
+twores = FALSE
+#resdirpref='CALIBRATION_GRID_20200305_092250/'
 #resdirpref='CALIBRATION_20201128_215412/'
-res2dirpref='CALIBRATION_NOCOUNTRY_GRID_20200423_172109/'
-generation='50000'
+resdirpref = '20220519_071436_CALIBRATION_NOCOUNTRY_GRID'
 
-res <- as.tbl(read.csv(paste0('openmole/calibration/',resdirpref,'/population',generation,'.csv')))
-res2 <- as.tbl(read.csv(paste0('openmole/calibration/',res2dirpref,'/population',generation,'.csv')))
+generation='55000'
+
+if(twores) res2dirpref='CALIBRATION_NOCOUNTRY_GRID_20200423_172109/'
+
+
+
+res <- as_tibble(read.csv(paste0('openmole/calibration/',resdirpref,'/population',generation,'.csv')))
 resdir=paste0('Results/Calibration/',resdirpref);dir.create(resdir)
-resdir2=paste0('Results/Calibration/',res2dirpref);dir.create(resdir2)
 
+minSamples = 20
+
+res = res[res$evolution.samples>=minSamples,]
+
+# add country gravity decay if not included in calibration (for genericity)
+if(is.null(res[['countryGravityDecay']])) res[['countryGravityDecay']] = rep(0,nrow(res))
+
+if (twores){
+  res2 <- as.tbl(read.csv(paste0('openmole/calibration/',res2dirpref,'/population',generation,'.csv')))
+  resdir2=paste0('Results/Calibration/',res2dirpref);dir.create(resdir2)
+  res2 = res2[res2$evolution.samples>=minSamples,]
+  allres = rbind(cbind(res,type=rep('full',nrow(res))),cbind(res2,type=rep('no countries',nrow(res2)),countryGravityDecay=rep(0,nrow(res2))))
+}
+  
 objectives = c("mselog","logmse")
 parameters = c("gravityDecay","countryGravityDecay","gammaSectors","gammaLinks","gammaOrigin","gammaDestination","finalTime")
-
-res = res[res$evolution.samples>=20,]
-res2 = res2[res2$evolution.samples>=20,]
-
-allres = rbind(cbind(res,type=rep('full',nrow(res))),cbind(res2,type=rep('no countries',nrow(res2)),countryGravityDecay=rep(0,nrow(res2))))
 
 paramnames = list(gammaOrigin = expression(gamma[O]),
                   gammaDestination = expression(gamma[D]),
@@ -35,28 +47,32 @@ paramnames = list(gammaOrigin = expression(gamma[O]),
                   finalTime=expression(t[f])
                   )
 
+# zoom? no
+#res = res[res$objective.logmse<36&res$objective.mselog<5.2,]
+
 # full model
 for(param in parameters){
-  g=ggplot(res,aes_string(x="logmse",y="mselog",color=param,size='evolution.samples'))
-  #g+geom_point(alpha=0.6)+xlab("log(Mean Squared Error)")+ylab("Mean Squared Error on log")+
-  #  scale_color_continuous(name=paramnames[[param]])+scale_size_continuous(name='Samples')+stdtheme
-  g+geom_point(alpha=0.6)+xlab(expression(epsilon[M]))+ylab(expression(epsilon[L]))+
-    scale_color_continuous(name=paramnames[[param]])+scale_size_continuous(name='Samples')+stdtheme
-    
-  ggsave(paste0(resdir,'/pareto_color',param,'.png'),width=20,height=18,units='cm')
+  ggsave(
+    ggplot(res,aes_string(x="objective.logmse",y="objective.mselog",color=param,size='evolution.samples'))+
+      geom_point(alpha=0.6)+xlab(expression(epsilon[M]))+ylab(expression(epsilon[L]))+
+        scale_color_continuous(name=paramnames[[param]])+scale_size_continuous(name='Samples')+stdtheme,
+      file=paste0(resdir,'/pareto_color',param,'.png'),width=20,height=18,units='cm'
+  )
 }
 
 # all
 
-for(param in parameters){
-  g=ggplot(allres,aes_string(x="logmse",y="mselog",color=param,size='evolution.samples',shape='type'))
-  g+geom_point(alpha=0.6)+xlab("log(Mean Squared Error)")+ylab("Mean Squared Error on log")+
+if (twores) {
+  for(param in parameters){
+    g=ggplot(allres,aes_string(x="logmse",y="mselog",color=param,size='evolution.samples',shape='type'))
+    g+geom_point(alpha=0.6)++xlab(expression(epsilon[M]))+ylab(expression(epsilon[L]))+
     scale_color_continuous(name=paramnames[[param]])+scale_size_continuous(name='Samples')+stdtheme
-  ggsave(paste0(resdir2,'/pareto_color',param,'.png'),width=20,height=18,units='cm')
+    ggsave(paste0(resdir2,'/pareto_color',param,'.png'),width=20,height=18,units='cm')
+  }
 }
-
+  
 ###
-bres = as.data.frame(res[res$mselog<5.0,])
+bres = as.data.frame(res[res$objective.mselog<5.0,])
 for(param in parameters){
   w = bres$evolution.samples/sum(bres$evolution.samples)
   p = sum(bres[,param]*w)
